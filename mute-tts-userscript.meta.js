0194b847654cc312ced821417086e8a350a2cb14
crashmax-dev <crashmax.dev@gmail.com>
2023-03-15 12:06:20 +0000

    Deploying to gh-pages from @ crashmax-dev/mute-tts-userscript@71ad74dbe325acfe3e3c89bba8755c4853406d9f 🚀

---
+++ mute-tts-userscript.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name        mute-tts-userscript
+// @version     0.0.1
+// @license     MIT
+// @homepage    https://crashmax-dev.github.io/mute-tts-userscript/
+// @match       https://www.twitch.tv/*
+// @grant       GM_addStyle
+// @updateURL   https://crashmax-dev.github.io/mute-tts-userscript/mute-tts-userscript.meta.js
+// @downloadURL https://crashmax-dev.github.io/mute-tts-userscript/mute-tts-userscript.user.js
+// ==/UserScript==
